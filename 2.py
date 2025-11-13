22e2ff46bdde0311f58264cdb4ec22ac8391c1e3
Fenyvesi <fenyvesijani97@gmail.com>
2025-11-13 10:50:54 +0100

    Kettes feladat leírása

---
+++ 2.py
@@ -0,0 +1,14 @@
+"""
+A program személyek nevét és annak éves átlagát kéri be. 
+
+Eldönti és kiírja, hogy a személy az átlag alapján hányas osztályzatot érdemel, 
+amit szövegesen meg is jelenít. 
+
+Az átlagból következő jegy 
+1-2 között elégtelen: 
+2,1-2,61 között: elégséges; 
+2,62-3,61 között: közepes; 
+3,62-4,61 között: 
+jó.4,62-5 között: jeles.
+
+"""
