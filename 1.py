23c5f3fb3d1663085792b169a4323a7938e355b6
Fenyvesi <fenyvesijani97@gmail.com>
2025-11-13 10:46:29 +0100

    Feladat megoldása

--- 1.py
+++ 1.py
@@ -2,3 +2,12 @@
 A program kérjen be a felhasználótól egy számot, 
 majd írja ki, hogy az osztható-e maradék nélkül 9-cel!
 """
+
+# 1. bekérés
+szam = int(input("Kérek egy számot: "))
+
+# 2. eldöntés
+if szam % 9 == 0:
+    print(f"A {szam} osztható 9-cel")
+else:
+    print(f"A {szam} nem osztható 9-cel")
